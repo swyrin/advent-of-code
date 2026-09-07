d028af27ca2483bd85fda05a50b5605a5eff0ded
Tien Dat Pham <dat20036@gmail.com>
2026-09-07 17:00:55 +0700

    making my life much harder.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,3 @@
 fn main() {
-    panic!("You should be running individual binaries.")
+    panic!("You should be running individual binaries, not this one. Yes, intentionally set this as default-run.")
 }
