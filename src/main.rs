997ff312387d8c756876be81de070c125dc20533
Tien Dat Pham <dat20036@gmail.com>
2026-09-17 10:51:19 +0700

    sometimes I kept running `cargo run`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,3 @@
 fn main() {
-    panic!("You should be running individual binaries, not this one. Yes, intentionally set this as default-run.")
+    panic!("You should be running individual binaries by `cargo run --bin xxx`, not `cargo run`.")
 }
