7445206f8ae9b7222282555ff5f39c4000994782
Tien Dat Pham <dat20036@gmail.com>
2026-09-23 13:58:05 +0700

    complete 2021 day 7 (part 1 only).
    I ain't spending hours to "mathing" the optimization for 1+2+3+4+... sum. It can wait.

---
+++ src/bin/year_2021_day_07.rs
@@ -0,0 +1,37 @@
+use macros::{AocInput, aoc};
+
+#[derive(AocInput)]
+struct Input {
+    #[parse(line(repeat_sep(u32, ",")))]
+    pub(crate) numbers: Vec<u32>,
+}
+
+aoc! {
+    #[sample(
+        input = "16,1,2,0,4,2,7,1,2,14",
+        expected = "37"
+    )]
+    fn part_one(Input { numbers }: &Input) -> impl std::fmt::Display {
+        let mut sorted = numbers.clone();
+        sorted.sort();
+
+        let middle = sorted.len() / 2;
+
+        // https://en.wikipedia.org/wiki/Geometric_median#Properties
+        let median = if sorted.len() % 2 == 0 {
+            (sorted[middle] + sorted[middle - 1]) / 2
+        } else {
+            sorted[middle]
+        };
+
+        numbers.iter().map(|x| x.abs_diff(median)).sum::<u32>()
+    }
+
+    #[sample(
+        input = "16,1,2,0,4,2,7,1,2,14",
+        expected = "168"
+    )]
+    fn part_two(Input { .. }: &Input) -> impl std::fmt::Display {
+        420
+    }
+}
